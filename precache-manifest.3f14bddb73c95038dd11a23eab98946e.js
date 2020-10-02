78a7a4377e3f34b14580c408a7e5d4ed6c9e6af7
HarishKumarI <harish@cognirel.com>
2020-10-02 22:36:04 +0530

    Updates

---
+++ precache-manifest.3f14bddb73c95038dd11a23eab98946e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74f0060d424acea3933b42ce57e2e9d3",
+    "url": "/chatbot-ui/index.html"
+  },
+  {
+    "revision": "836dd8d50ec3e48f0cf6",
+    "url": "/chatbot-ui/static/css/main.11f70a66.chunk.css"
+  },
+  {
+    "revision": "a44a6ac9f720c16f357a",
+    "url": "/chatbot-ui/static/js/2.8ac7c4a3.chunk.js"
+  },
+  {
+    "revision": "4b9f2ea8a4aec612078d123c18f7727d",
+    "url": "/chatbot-ui/static/js/2.8ac7c4a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "836dd8d50ec3e48f0cf6",
+    "url": "/chatbot-ui/static/js/main.6c029811.chunk.js"
+  },
+  {
+    "revision": "dc39b43a79bd4a11ec92",
+    "url": "/chatbot-ui/static/js/runtime-main.b3495b85.js"
+  }
+]);
