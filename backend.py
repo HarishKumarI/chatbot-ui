385e39e589316b19d9948648e48cf4d3157f6f03
HarishKumarI <harish@cognirel.com>
2020-11-17 10:57:15 +0530

    backend.py update

--- backend.py
+++ backend.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response, jsonify, send_file, send_from_directory
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
 
@@ -9,6 +9,8 @@ app = Flask(__name__)
 
 CORS(app)
 
+
+
 @app.route('/api/session_list',methods=['GET'])
 def SessionList():
     if(request.method == 'GET'):
@@ -16,10 +18,25 @@ def SessionList():
         
         res = r.keys()   
         
-        session_list = [x.decode('utf-8')[8:] for x in res if x.decode('utf-8').startswith('session:')]
-                    
+        session_list = []
+        
+        for x in res:
+            
+            ses = x.decode('utf-8')
+            
+            if(ses.startswith('session:')):
+                
+                ses_res_pkl = r.get(ses)
+                ses_res = pickle.loads(ses_res_pkl)
+                session_list.append({
+                    'session_id': ses[8:],
+                    'user_id': ses_res.get('user_id', None),
+                    'created_at': ses_res.get('created_at', None)
+                })
 
         return jsonify({'session_list': session_list})
+
+
 
 
 @app.route('/api/session',methods=['POST'])
@@ -40,10 +57,11 @@ def SessionData():
         print( result['history'] )
                     
 
-        return jsonify(result['history'])
+        return jsonify({'history': result['history'], 'user_id': result.get('user_id', None)})
 #         except Exception as e:
 #             print(e)
 #             return jsonify( {'msg':'error', 'data': {} } )
+
 
 
 if __name__ == '__main__':
